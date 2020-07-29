4fe55815bac0133abe08ce47fc4c403ddef2d5ae
Efi Gordon <efigordon90@gmail.com>
2020-07-29 21:23:05 +0300

    mm

--- components/comments.js
+++ components/comments.js
@@ -22,8 +22,3 @@ export default function Comments(commentsArray) {
     )
 }
 
-export async function getServerSideProps(context) {
-    return {
-        props: {}, // will be passed to the page component as props
-    }
-}
